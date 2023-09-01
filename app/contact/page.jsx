8e1cf9ff259a918bcb75a01dde802b07a6fc7db0
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-09-02 00:19:03 +0400

    updated contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,21 +2,23 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col m-20">
-      <h1 className="font-didact text-4xl font-bold flex justify-center">Contact Me</h1>
+    <div className="flex flex-col m-20 max-[1000px]:flex-col">
+      <h1 className="font-didact text-4xl font-bold flex justify-center">
+        Contact Me
+      </h1>
       <br />
-      <p className="font-didact text-3xl font-bold w-6/12 m-auto">
-        <span>Email: joseph.mccain28@gmail.com</span>
+      <p className="font-didact text-4xl font-bold flex flex-col justify-center items-center max-[1000px]:text-lg">
+        Email: joseph.mccain28@gmail.com
         <br />
-        <span>Phone: (302) 723-6573</span>
+        Phone: (302) 723-6573
         <br />
-        <span>Address: 17-11 Hancock Street, Queens, NY</span>
+        Address: 17-11 Hancock Street, Queens, NY
         <br />
-        <span>dalmatianofficial.com</span>
+        dalmatianofficial.com
         <br />
-        <span>instagram.com/josephmccainn</span>
+        instagram.com/josephmccainn
         <br />
-        <span>instagram.com/laeliagardin</span>
+        instagram.com/laeliagardin
       </p>
     </div>
   );
